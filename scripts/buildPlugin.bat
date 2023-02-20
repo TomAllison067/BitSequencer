40d10c10365413e6b9bd397ee81504e68eb00df2
Tom <tomlaurenceallison@gmail.com>
2023-02-20 21:14:14 +0000

    Update scripts and esos

--- scripts/buildPlugin.bat
+++ scripts/buildPlugin.bat
@@ -1,1 +1,2 @@
-javac --module-path "c:\openJFX\javafx-sdk-14.0.2.1\lib" --add-modules javafx.controls -d . -cp %artHome%/art.jar ValueUserPlugin.java
+javac -cp BeatSequencer BeatSequencer/MiniMusicPlayer.java
+javac -cp ART/art.jar ValueUserPlugin.java
--- scripts/buildPlugin.sh
+++ scripts/buildPlugin.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 javac  -cp ".:BeatSequencer" BeatSequencer/MiniMusicPlayer.java
-javac --module-path /usr/share/openjfx/lib --add-modules javafx.controls -cp ".:./ART/art.jar:MidiPlayer" ValueUserPlugin.java
+javac  -cp ".:./ART/art.jar" ValueUserPlugin.java
